b4d5b1395f76a1d6a41dbc07f2c1ede73f958f98
xnatnat <mota.nathalia963@gmail.com>
2023-04-26 13:45:50 -0300

    initial commit

---
+++ src/main/java/br/com/chronosapi/ChronosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.chronosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/chronosapi/ChronosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.chronosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
